cdabfb7bf6496baed633b1795d01d6fb76deca65
Massimo Battestini <massimob76@gmail.com>
2016-05-15 22:59:28 +0100

    adds endpoint and integration test

---
+++ src/itest/java/api/PlayApiTest.java
@@ -0,0 +1,27 @@
+package api;
+
+import org.junit.Test;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class PlayApiTest {
+
+    private static final String URI = "http://localhost:8090";
+    private static final String PING_PATH = "/ping";
+
+    @Test
+    public void serverIsUpTest() {
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target(URI).path(PING_PATH);
+        Response response = target.request().get();
+        assertThat(response.getStatus(), is(200));
+        assertThat(response.readEntity(String.class), is("pong"));
+    }
+
+}
