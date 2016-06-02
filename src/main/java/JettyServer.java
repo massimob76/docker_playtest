96f471a17ba80c1589f50776db70eb420f867caf
Massimo Battestini <massimo.battestini@king.com>
2016-06-02 22:42:11 +0100

    separated jettyserver logic into dedicated class

---
+++ src/main/java/JettyServer.java
@@ -0,0 +1,41 @@
+import com.google.inject.Inject;
+import conf.PropertiesReader;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.servlet.ServletContainer;
+
+public class JettyServer {
+
+    private static final String SERVER_PORT = "serverPort";
+
+    private final PropertiesReader propertiesReader;
+    private Server server;
+    private ServletContextHandler context;
+
+    @Inject
+    public JettyServer(PropertiesReader propertiesReader) {
+        this.propertiesReader = propertiesReader;
+    }
+
+    public void start() throws Exception {
+        int port = Integer.parseInt(propertiesReader.getProperty(SERVER_PORT));
+
+        server = new Server(port);
+        context = new ServletContextHandler();
+        server.setHandler(context);
+
+        server.start();
+    }
+
+    public void registerServlet(Object... servlets) {
+        ResourceConfig resourceConfig = new ResourceConfig().registerInstances(servlets);
+        ServletHolder servletHolder = new ServletHolder(new ServletContainer(resourceConfig));
+        context.addServlet(servletHolder, "/*");
+    }
+
+    public void join() throws InterruptedException {
+        server.join();
+    }
+}
--- src/main/java/Startup.java
+++ src/main/java/Startup.java
@@ -1,11 +1,6 @@
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import conf.PropertiesReader;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
-import org.glassfish.jersey.server.ResourceConfig;
-import org.glassfish.jersey.servlet.ServletContainer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import servlet.PlaytestServlet;
@@ -13,7 +8,6 @@ import servlet.PlaytestServlet;
 public class Startup {
 
     private static final String DEFAULT_PROPERTY_FILE = "config.properties";
-    private static final String SERVER_PORT = "serverPort";
     private static final String SERVER_DAEMON = "serverDaemon";
     private static final String STARTED_APPLICATION_FLAG = "Started Application";
     private static final Logger LOGGER = LoggerFactory.getLogger(Startup.class);
@@ -23,26 +17,17 @@ public class Startup {
         PropertiesReader propertiesReader = new PropertiesReader(propertyFile);
         Injector injector = Guice.createInjector(new PlaytestModule(propertiesReader));
 
-        int port = Integer.parseInt(propertiesReader.getProperty(SERVER_PORT));
-        boolean isDaemon = Boolean.parseBoolean(propertiesReader.getProperty(SERVER_DAEMON));
-
-        Server server = new Server(port);
+        JettyServer jettyServer = injector.getInstance(JettyServer.class);
+        jettyServer.start();
 
         PlaytestServlet playtestServlet = injector.getInstance(PlaytestServlet.class);
-        ResourceConfig resourceConfig = new ResourceConfig().registerInstances(playtestServlet);
+        jettyServer.registerServlet(playtestServlet);
 
-        ServletContextHandler context = new ServletContextHandler();
-        server.setHandler(context);
-
-        ServletHolder servletHolder = new ServletHolder(new ServletContainer(resourceConfig));
-        context.addServlet(servletHolder, "/*");
-
-        server.start();
-        server.dumpStdErr();
         LOGGER.info(STARTED_APPLICATION_FLAG);
 
+        boolean isDaemon = Boolean.parseBoolean(propertiesReader.getProperty(SERVER_DAEMON));
         if (!isDaemon) {
-            server.join();
+            jettyServer.join();
         }
     }
 
