1fee71f80c00cb6f4ea3c2488ccd6f1f639707a8
Massimo Battestini <massimo.battestini@king.com>
2016-06-04 19:34:09 +0100

    starting up h2 db as part of the integration test

--- src/main/java/Startup.java
+++ src/main/java/Startup.java
@@ -1,9 +1,12 @@
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import conf.PropertiesReader;
+import org.h2.tools.Server;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import servlet.PlaytestServlet;
+
+import java.sql.SQLException;
 
 public class Startup {
 
@@ -11,17 +14,33 @@ public class Startup {
     private static final String SERVER_DAEMON = "serverDaemon";
     private static final String STARTED_APPLICATION_FLAG = "Started Application";
     private static final Logger LOGGER = LoggerFactory.getLogger(Startup.class);
+    private static final String USE_H2_DB = "useH2DB";
 
     public void start(String propertyFile) throws Exception {
-
         PropertiesReader propertiesReader = new PropertiesReader(propertyFile);
         Injector injector = Guice.createInjector(new PlaytestModule(propertiesReader));
 
+        boolean useH2DB = Boolean.parseBoolean(propertiesReader.getProperty(USE_H2_DB));
+        if (useH2DB) {
+            startH2DBServer();
+        }
+
         JettyServer jettyServer = injector.getInstance(JettyServer.class);
+        PlaytestServlet playtestServlet = injector.getInstance(PlaytestServlet.class);
+        startServer(jettyServer, propertiesReader, playtestServlet);
+
+    }
+
+    private void startH2DBServer() throws SQLException {
+        Server server = Server.createWebServer("-webDaemon");
+        server.start();
+        LOGGER.info("started h2 DB server");
+    }
+
+    private void startServer(JettyServer jettyServer, PropertiesReader propertiesReader, Object... servlets) throws Exception {
         jettyServer.start();
 
-        PlaytestServlet playtestServlet = injector.getInstance(PlaytestServlet.class);
-        jettyServer.registerServlet(playtestServlet);
+        jettyServer.registerServlet(servlets);
 
         LOGGER.info(STARTED_APPLICATION_FLAG);
 
